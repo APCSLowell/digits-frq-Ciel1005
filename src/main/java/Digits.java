9450d1e0cf78022c024685bc39b627d03208cba9
Ciel1005 <143016585+Ciel1005@users.noreply.github.com>
2024-04-14 14:52:08 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,17 +7,18 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
+		digitList = new ArrayList<Ingeger>();
 	    String a = num + "";
-		for(int i = 0; i < a.length()-1; i++)
-			digitList.add(Integer.parseInt(a.substring(i, i+1)));
+		for(int i = 0; i < a.length(); i++)
+			digitList.add(string.charAt(i) - '0');
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i = 0; i < digitList.size()-1;i++)
-			if(digitList.get(i+1) > digitList.get(i))
-			   return true;
-		return false;
+			if(digitList.get(i+1)<=digitList.get(i))
+			   return false;
+		return true;
 	}
 	
 	public String toString()
